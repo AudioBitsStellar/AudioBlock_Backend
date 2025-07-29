1845f9e26cfcbc26988ba02947108fa6f8fe741f
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2025-07-29 15:41:13 +0100

    Fix redis production error 2

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,11 +4,12 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || "6379"),
   password: process.env.REDIS_PASSWORD,
-  tls: {}, // Redis Cloud requires TLS
+  // tls: {}, // Required for Redis Cloud TLS
+  maxRetriesPerRequest: null, //  prevents retry errors
 });
 
 redis.on("connect", () => {
-  console.log("✅ Connected to Redis Cloud");
+  console.log("✅ Connected to Redis");
 });
 
 redis.on("error", (err) => {
